538e98f05a509ad93a0dab23ac462809769a404e
zhangty <465355874@qq.com>
2019-08-15 09:10:41 +0800

    app启动类

---
+++ concurrency/src/main/java/zty/practise/concurrency/ConcurrencyApp.java
@@ -0,0 +1,12 @@
+package zty.practise.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApp.class, args);
+	}
+}
